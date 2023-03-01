579fedc27d671557f61f8f473c03d2f6c907e6d2
Garbi <francogarbi85@gmail.com>
2023-02-28 22:53:19 -0300

    Ultimo commit

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ descarga.addEventListener("click",()=>{
 window.print();
 
 })
-
-
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
+  function myFunction() {
+    alert("BIENVENIDOS A MI CV!");
+  }
 
